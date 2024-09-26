9688adfaceacaa66038625c6a785451671ef0223
ruamsuk <ruamsuk_w@hotmail.com>
2024-09-26 11:28:16 +0700

    initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"angular-firebase-app-1a38c","appId":"1:901081309954:web:aa619c6c4007c5e73dd81b","databaseURL":"https://angular-firebase-app-1a38c-default-rtdb.asia-southeast1.firebasedatabase.app","storageBucket":"angular-firebase-app-1a38c.appspot.com","locationId":"asia-southeast1","apiKey":"AIzaSyAFyLWIEdvfilRgEgKYU-ujpEJFc97c00k","authDomain":"angular-firebase-app-1a38c.firebaseapp.com","messagingSenderId":"901081309954"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
+  ],
 };
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,60 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DialogModule } from 'primeng/dialog';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { CardModule } from 'primeng/card';
+import { ToastModule } from 'primeng/toast';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuModule } from 'primeng/menu';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { TableModule } from 'primeng/table';
+import { InputIconModule } from 'primeng/inputicon';
+import { IconFieldModule } from 'primeng/iconfield';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { CalendarModule } from 'primeng/calendar';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { TreeSelectModule } from 'primeng/treeselect';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+
+@NgModule({
+  exports: [
+    AvatarModule,
+    AutoCompleteModule,
+    CommonModule,
+    CardModule,
+    TreeSelectModule,
+    CalendarModule,
+    ConfirmPopupModule,
+    ConfirmDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DialogModule,
+    DynamicDialogModule,
+    ButtonModule,
+    InputTextModule,
+    InputMaskModule,
+    InputNumberModule,
+    InputSwitchModule,
+    PasswordModule,
+    TableModule,
+    ToastModule,
+    MenuModule,
+    MenubarModule,
+    InputTextareaModule,
+    InputIconModule,
+    IconFieldModule,
+    ProgressSpinnerModule,
+    FloatLabelModule,
+  ],
+})
+export class SharedModule {}
